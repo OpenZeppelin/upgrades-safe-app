39af58b3d866e7b996ed66e5ba943f772e819c3c
Martin Triay <martriay@gmail.com>
2020-05-30 03:22:10 -0300

    Render DefenderSafe component if safe is loaded

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App: React.FC = () => {
 
   return (
     <div>
-      {( ! safe.info
+      {( safe.info
         ? <DefenderSafe providers={{ web3, safe }} />
         : <>
           Loading...<br />
